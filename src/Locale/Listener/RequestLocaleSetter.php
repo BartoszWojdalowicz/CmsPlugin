a59009fba4370f7ee59a25f0a77b2e0d0ebe94d1
Bartosz Wojdałowicz <bartoszwojdalowicz@gmail.com>
2026-05-04 09:47:48 +0200

    copy and adjust local component

---
+++ src/Locale/Listener/RequestLocaleSetter.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\CmsPlugin\Locale\Listener;
+
+
+use Sylius\CmsPlugin\Locale\Context\LocaleContextInterface;
+use Sylius\CmsPlugin\Locale\Context\LocaleNotFoundException;
+use Sylius\CmsPlugin\Locale\Provider\LocaleProviderInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+final class RequestLocaleSetter
+{
+    public function __construct(private LocaleContextInterface $localeContext, private LocaleProviderInterface $localeProvider)
+    {
+    }
+
+    /**
+     * @throws LocaleNotFoundException
+     */
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+
+        $request->setLocale($this->localeContext->getLocaleCode());
+        $request->setDefaultLocale($this->localeProvider->getDefaultLocaleCode());
+    }
+}
---
+++ src/Locale/Twig/LocaleExtension.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\CmsPlugin\Locale\Twig;
+
+
+use Sylius\CmsPlugin\Locale\Context\LocaleContextInterface;
+use Sylius\CmsPlugin\Locale\Context\LocaleNotFoundException;
+use Sylius\CmsPlugin\Locale\Converter\LocaleConverterInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+final class LocaleExtension extends AbstractExtension
+{
+    public function __construct(
+        private LocaleConverterInterface $localeConverter,
+        private LocaleContextInterface $localeContext,
+    ) {
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('sylius_locale_name', [$this, 'convertCodeToName']),
+            new TwigFilter('sylius_locale_country', [$this, 'getCountryCode']),
+        ];
+    }
+
+    public function convertCodeToName(string $code, ?string $localeCode = null): string
+    {
+        try {
+            return $this->localeConverter->convertCodeToName($code, $this->getLocaleCode($localeCode));
+        } catch (\InvalidArgumentException) {
+            return $code;
+        }
+    }
+
+    public function getLocaleCode(?string $localeCode): ?string
+    {
+        if (null !== $localeCode) {
+            return $localeCode;
+        }
+
+        try {
+            return $this->localeContext->getLocaleCode();
+        } catch (LocaleNotFoundException) {
+            return null;
+        }
+    }
+
+    public function getCountryCode(string $locale): ?string
+    {
+        return \Locale::getRegion($locale);
+    }
+}
