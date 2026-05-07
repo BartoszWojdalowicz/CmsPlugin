529c676621f47c6218b2b7c3bba6057ae8d00f54
Bartosz Wojdałowicz <bartoszwojdalowicz@gmail.com>
2026-05-07 11:56:33 +0200

    CR fixes

--- src/Locale/Twig/LocaleExtension.php
+++ src/Locale/Twig/LocaleExtension.php
@@ -13,52 +13,16 @@ declare(strict_types=1);
 
 namespace Sylius\CmsPlugin\Locale\Twig;
 
-use Sylius\CmsPlugin\Locale\Context\LocaleContextInterface;
-use Sylius\CmsPlugin\Locale\Context\LocaleNotFoundException;
-use Sylius\CmsPlugin\Locale\Converter\LocaleConverterInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
 final class LocaleExtension extends AbstractExtension
 {
-    public function __construct(
-        private LocaleConverterInterface $localeConverter,
-        private LocaleContextInterface $localeContext,
-    ) {
-    }
-
     public function getFilters(): array
     {
         return [
-            new TwigFilter('sylius_locale_name', [$this, 'convertCodeToName']),
-            new TwigFilter('sylius_locale_country', [$this, 'getCountryCode']),
+            new TwigFilter('sylius_locale_name', [LocaleRuntime::class, 'convertCodeToName']),
+            new TwigFilter('sylius_locale_country', [LocaleRuntime::class, 'getCountryCode']),
         ];
-    }
-
-    public function convertCodeToName(string $code, ?string $localeCode = null): string
-    {
-        try {
-            return $this->localeConverter->convertCodeToName($code, $this->getLocaleCode($localeCode));
-        } catch (\InvalidArgumentException) {
-            return $code;
-        }
-    }
-
-    public function getLocaleCode(?string $localeCode): ?string
-    {
-        if (null !== $localeCode) {
-            return $localeCode;
-        }
-
-        try {
-            return $this->localeContext->getLocaleCode();
-        } catch (LocaleNotFoundException) {
-            return null;
-        }
-    }
-
-    public function getCountryCode(string $locale): ?string
-    {
-        return \Locale::getRegion($locale);
     }
 }
